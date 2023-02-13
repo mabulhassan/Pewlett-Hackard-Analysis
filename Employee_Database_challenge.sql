e21c0ad4726884970d22660077de67dd04239849
Maha Abul-Hassan <maha3220@yahoo.com>
2023-02-13 12:17:34 -0500

    PH analysis sql

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,46 @@
+
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no, 
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles 
+FROM retirement_titles AS rt
+WHERE  rt.to_date = '9999-01-01'
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
+
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility 
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date  = '9999-01-01')  
+ORDER BY e.emp_no, to_date DESC;  
